fee3b9c0d8019b9c8d93e73325d4817a216675f8
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2018-12-11 14:01:24 +0900

    Add BitcoinService class

--- lib/lightning/blockchain.rb
+++ lib/lightning/blockchain.rb
@@ -2,6 +2,7 @@
 
 module Lightning
   module Blockchain
+    autoload :BitcoinService, 'lightning/blockchain/bitcoin_service'
     autoload :Messages, 'lightning/blockchain/messages'
     autoload :Wallet, 'lightning/blockchain/wallet'
     autoload :Watcher, 'lightning/blockchain/watcher'
---
+++ lib/lightning/blockchain/bitcoin_service.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require 'jsonclient'
+
+module Lightning
+  module Blockchain
+    class BitcoinService
+      def generate_new_address(account_name)
+        create_account(account_name)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'getnewaddress',
+          'params': [account_name]
+        }
+        address = client.post(url, params).body
+        { address: address }
+      end
+
+      def create_account(account_name)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'createaccount',
+          'params': [account_name]
+        }
+        client.post(url, params).body
+      end
+
+      def blockchain_info
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'getblockchaininfo'
+        }
+        client.post(url, params).body
+      end
+
+      def get_balance(account_name)
+        create_account(account_name)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'getbalance',
+          'params': [account_name]
+        }
+        client.post(url, params).body
+      end
+
+      def list_unspent(account_name)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'listunspentinaccount',
+          'params': [account_name]
+        }
+        client.post(url, params).body
+      end
+
+      def signrawtransaction(account_name, tx)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'signrawtransaction',
+          'params': [account_name, tx.to_payload.bth]
+        }
+        client.post(url, params).body
+      end
+
+      def broadcast(tx)
+        client = JSONClient.new
+        client.debug_dev = STDOUT
+        url = build_bitcoin_rpc_url
+        params = {
+          'method': 'sendrawtransaction',
+          'params': [tx.to_payload.bth]
+        }
+        client.post(url, params).body
+      end
+
+      def build_bitcoin_rpc_url
+        'http://localhost:18443/'
+      end
+    end
+  end
+end
