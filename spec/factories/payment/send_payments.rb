eaa9a05799945232a1d8f92d95fe8632367af74d
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2019-02-12 15:59:29 +0900

    Fix rspec warnings

--- spec/factories/payment/send_payments.rb
+++ spec/factories/payment/send_payments.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     amount_msat { 10_000_000 }
     payment_hash { '00' * 32 }
     target_node_id { '028d7500dd4c12685d1f568b4c2b5048e8534b873319f3a8daa612b469132ec7f7' }
-    routes []
-    final_cltv_expiry 144
+    routes { [] }
+    final_cltv_expiry { 144 }
     initialize_with do
       new(Lightning::Payment::Messages::SendPayment[amount_msat, payment_hash, target_node_id, routes, final_cltv_expiry])
     end
