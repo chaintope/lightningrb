5c719aad57c565383e8a90d55b314ac7f56ae9b3
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2019-03-13 20:51:16 +0900

    Implement methods for BOLT #9: Assigned Feature Flags

--- lib/lightning/feature.rb
+++ lib/lightning/feature.rb
@@ -1,23 +1,63 @@
 # frozen_string_literal: true
 
 module Lightning
-  module Feature
-    INITIAL_ROUTING_SYNC_BIT_MANDATORY = 2
+  class Feature
+    # Requires or supports extra channel_reestablish fields
+    OPTION_DATA_LOSS_PROTECT_COMPULSORY = 0
+    OPTION_DATA_LOSS_PROTECT_OPTIONAL = 1
+
+    # Indicates that the sending node needs a complete routing information dump
+    # INITIAL_ROUTING_SYNC_BIT_COMPULSORY = 2
     INITIAL_ROUTING_SYNC_BIT_OPTIONAL = 3
-    def self.initial_routing_sync?(features)
-      features.to_i[INITIAL_ROUTING_SYNC_BIT_OPTIONAL] == 1
+
+    # Commits to a shutdown scriptpubkey when opening channel
+    OPTION_UPFRONT_SHUTDOWN_SCRIPT_COMPULSORY = 4
+    OPTION_UPFRONT_SHUTDOWN_SCRIPT_OPTIONAL = 5
+
+    # More sophisticated gossip control
+    GOSSIP_QUERY_COMPULSORY = 6
+    GOSSIP_QUERY_OPTIONAL = 7
+
+    def initialize(flags_hex_string)
+      @flags = flags_hex_string.to_i(16)
     end
 
-    def self.supported?(features)
-      bits = features.htb.unpack('b*').first
-      bits.size.times do |i|
-        return false if required_bit?(bits, i)
+    def option_data_loss_protect?
+      has_feature?(OPTION_DATA_LOSS_PROTECT_COMPULSORY) ||
+      has_feature?(OPTION_DATA_LOSS_PROTECT_OPTIONAL)
+    end
+
+    def initial_routing_sync?
+      has_feature?(INITIAL_ROUTING_SYNC_BIT_OPTIONAL)
+    end
+
+    def option_upfront_shutdown_script?
+      has_feature?(OPTION_UPFRONT_SHUTDOWN_SCRIPT_COMPULSORY) ||
+      has_feature?(OPTION_UPFRONT_SHUTDOWN_SCRIPT_OPTIONAL)
+    end
+
+    def gossip_queries?
+      has_feature?(GOSSIP_QUERY_COMPULSORY) ||
+      has_feature?(GOSSIP_QUERY_OPTIONAL)
+    end
+
+    def valid?
+      mask = (1 << OPTION_DATA_LOSS_PROTECT_COMPULSORY) +
+        (1 << OPTION_UPFRONT_SHUTDOWN_SCRIPT_COMPULSORY) +
+        (1 << GOSSIP_QUERY_COMPULSORY)
+
+      flags = @flags & ~mask
+      while flags > 0
+        return false if flags & 1 == 1
+        flags = flags >> 2
       end
       true
     end
 
-    def self.required_bit?(bits, i)
-      i.even? && bits[i] == '1'
+    private
+
+    def has_feature?(feature_bit)
+      @flags & (1 << feature_bit) != 0
     end
   end
 end
--- spec/lightning/feature_spec.rb
+++ spec/lightning/feature_spec.rb
@@ -3,13 +3,38 @@
 require 'spec_helper'
 
 describe Lightning::Feature do
-  describe '.initial_routing_sync?' do
-    it { expect(Lightning::Feature.initial_routing_sync?('08')).to eq true }
+  describe '#option_data_loss_protect?' do
+    it { expect(Lightning::Feature.new('01').option_data_loss_protect?).to eq true }
+    it { expect(Lightning::Feature.new('02').option_data_loss_protect?).to eq true }
+    it { expect(Lightning::Feature.new('04').option_data_loss_protect?).to eq false }
   end
-  describe '.supported?' do
-    it { expect(Lightning::Feature.supported?('0400000000000000')).to eq false }
-    it { expect(Lightning::Feature.supported?('0800000000000000')).to eq true }
-    it { expect(Lightning::Feature.supported?('14')).to eq false }
-    it { expect(Lightning::Feature.supported?('0141')).to eq false }
+
+  describe '#initial_routing_sync?' do
+    it { expect(Lightning::Feature.new('04').initial_routing_sync?).to eq false }
+    it { expect(Lightning::Feature.new('08').initial_routing_sync?).to eq true }
+    it { expect(Lightning::Feature.new('10').initial_routing_sync?).to eq false }
+  end
+
+  describe '#option_upfront_shutdown_script?' do
+    it { expect(Lightning::Feature.new('10').option_upfront_shutdown_script?).to eq true }
+    it { expect(Lightning::Feature.new('20').option_upfront_shutdown_script?).to eq true }
+    it { expect(Lightning::Feature.new('40').option_upfront_shutdown_script?).to eq false }
+  end
+
+  describe '#gossip_queries?' do
+    it { expect(Lightning::Feature.new('40').gossip_queries?).to eq true }
+    it { expect(Lightning::Feature.new('80').gossip_queries?).to eq true }
+    it { expect(Lightning::Feature.new('0100').gossip_queries?).to eq false }
+  end
+
+  describe '#valid?' do
+    # The 2nd bits(even) of '14' is unknown, since there is no even bit for initial_routing_sync
+    it { expect(Lightning::Feature.new('14').valid?).to eq false }
+
+    # The 8th bits of '0141' is unknown
+    it { expect(Lightning::Feature.new('0141').valid?).to eq false }
+
+    # The 9th bits of '0200' is unknown, but it is optional bit
+    it { expect(Lightning::Feature.new('0200').valid?).to eq true }
   end
 end
