174deec74a56124953ff1d5f4953140cf2c09952
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2019-04-22 15:15:15 +0900

    Remove unused methods

--- lib/lightning/channel/helpers.rb
+++ lib/lightning/channel/helpers.rb
@@ -3,25 +3,6 @@
 module Lightning
   module Channel
     module Helpers
-      include Algebrick::Matching
-
-      def get_channel_id(data)
-        match data,
-              (on DataWaitForOpenChannel do
-                data.init_fundee.temporary_channel_id
-              end), (on DataWaitForAcceptChannel do
-                data.init_funder.temporary_channel_id
-              end), (on DataWaitForFundingInternal do
-                data.temporary_channel_id
-              end), (on DataWaitForFundingCreated do
-                data.temporary_channel_id
-              end), (on DataWaitForFundingSigned do
-                data.channel_id
-              end), (on HasCommitments do
-                data.channel_id
-              end)
-      end
-
       def self.final_script_pubkey(context)
         address = context.spv.generate_new_address(context.node_params.node_id)
         Bitcoin::Script.parse_from_addr(address).to_payload.bth
