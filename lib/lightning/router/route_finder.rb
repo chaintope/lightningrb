c85f8294f68f615681da3d81888fc83992d57edf
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2019-07-29 18:50:43 +0900

    Fix finding routes with short_channel_ids

--- lib/lightning/router/route_finder.rb
+++ lib/lightning/router/route_finder.rb
@@ -9,15 +9,19 @@ module Lightning
       # @param target String public key
       # @param updates Hash whose key is ChannelDesc and value is ChannelUpdate
       # @param assisted_routes Array of public keys
-      # @param assisted_channels Array of short_channel_id
+      # @param channels Array of short_channel_id
       # @return
-      def self.find(source, target, updates, _assisted_routes, assisted_channels)
-        routes = updates.map do |k, _v|
-          [k[:a], k[:b], 1]
-        end
-        assisted_channels.map do |short_channel_id|
-          key = updates.select { |k, _v| k[:id] == short_channel_id }.keys.first
-          routes << [key[:a], key[:b], 0] if key
+      def self.find(source, target, updates, _assisted_routes, channels)
+        routes = []
+        if channels.empty?
+          routes += updates.map do |k, _v|
+            [k[:a], k[:b], 0]
+          end
+        else
+          channels.map do |short_channel_id|
+            key = updates.select { |k, _v| k[:id] == short_channel_id }.keys.first
+            routes << [key[:a], key[:b], 0] if key
+          end
         end
         graph = Dijkstraruby::Graph.new(routes)
         result = graph.shortest_path(source, target)
@@ -27,6 +31,8 @@ module Lightning
           next unless paths[index + 1]
           [pubkey, paths[index + 1]]
         end.compact
+
+        updates = updates.select { |k, _v| channels.include?(k[:id]) } unless channels.empty?
 
         hops.map do |hop|
           desc = updates.select { |k, _v| (k[:a] == hop[0] && k[:b] == hop[1])||(k[:a] == hop[1] && k[:b] == hop[0]) }.keys.first
